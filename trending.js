92417b31e7786d54caa6a0771dc625031ffff40e
Stefanos Chrs <stefanoschrs@hotmail.com>
2016-05-19 21:14:56 +0300

    URL added

--- trending.js
+++ trending.js
@@ -1,13 +1,15 @@
 function parse(html){
-	var $ = require('cheerio').load(html)
+	var $ = require('cheerio').load(html);
 	var repositories = [];
 	$('.repo-list-item').each(function(index, element){
-		var title = $(element).find('.repo-list-name').children()[0].attribs.href.trim().split('/');
+		var url = $(element).find('.repo-list-name').children()[0].attribs.href.trim();
+		var title = url.split('/');
 		var description = $(element).find('.repo-list-description').text().trim();
 		var meta = $(element).find('.repo-list-meta').text().split('•');
 
 		var item = {
 			author: title[1],
+			url: `https://github.com${url}`,
 			title: title[2],
 			description: description,
 			language: meta[0].trim(),
@@ -20,13 +22,14 @@ function parse(html){
 		repositories.forEach(function(repo, index){
 			console.log(`${index+1}. ${repo.title} (${repo.author}) - ${repo.language} - ${repo.stars}`);
 			console.log(`\t${repo.description}`);
+			console.log(`\t${repo.url}`);
 		});
 	}
 	else if(outputType === 'json'){
 		console.log(JSON.stringify(repositories));
 	}
 	else if(outputType === 'csv'){
-		console.log('Author,Title,Description,Language,Stars');
+		console.log('Author,Url,Title,Description,Language,Stars');
 		repositories.forEach(repo => {
 			console.log(Object.keys(repo).map(key => repo[key]).join(','))
 		});
